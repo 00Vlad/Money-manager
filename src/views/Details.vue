1e1c4b383a6e38072fefa1747b028d1d8b4c94d3
Vlad <miegeff@gmail.com>
2020-07-03 12:48:23 +0300

    Full History

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -2,26 +2,55 @@
   <main class="app-content">
     <div class="app-page">
       <div>
-        <div>
+        <Loader v-if="loading"/>
+        <div v-else-if="record">
           <div class="breadcrumb-wrap">
-            <a href="/history" class="breadcrumb">История</a>
-            <a class="breadcrumb">Расход</a>
+            <router-link to="/history" class="breadcrumb">History</router-link>
+            <a @click.prevent class="breadcrumb">
+              {{ record.type === 'income' ? 'Income' : 'Outcome' }}
+            </a>
           </div>
           <div class="row">
             <div class="col s12 m6">
-              <div class="card red">
+              <div class="card" :class="{
+                  'red': record.type === 'outcome',
+                  'green' : record.type === 'income'
+                  }">
                 <div class="card-content white-text">
-                  <p>Описание:</p>
-                  <p>Сумма:</p>
-                  <p>Категория:</p>
+                  <p>Description: {{ record.description }}</p>
+                  <p>Amount: {{ record.amount | currencyFilter('PLN') }}</p>
+                  <p>Category: {{ record.categoryName }}</p>
 
-                  <small>12.12.12</small>
+                  <small>{{ record.date | dateFilter('datetime') }}</small>
                 </div>
               </div>
             </div>
           </div>
         </div>
+        <p v-else class="center">No such record</p>
       </div>
     </div>
   </main>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    name: 'detail',
+    record: null,
+    loading: true
+  }),
+  async mounted () {
+    const id = this.$route.params.id
+    const record = await this.$store.dispatch('fetchRecordById', id)
+    // eslint-disable-next-line no-unused-vars
+    const category = await this.$store.dispatch('fetchCategoryById', record.categoryId)
+
+    this.record = {
+      ...record,
+      categoryName: category.title
+    }
+    this.loading = false
+  }
+}
+</script>
